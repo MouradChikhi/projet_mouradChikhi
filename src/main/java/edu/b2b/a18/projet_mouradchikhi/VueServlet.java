ddafc2c1a9ea44911fcd309bd080b1713c934bbe
Mourad Chikhi <6286227@bdeb.qc.ca>
2025-03-12 13:25:36 -0400

    Ajout de vue.js et VueServlet

---
+++ src/main/java/edu/b2b/a18/projet_mouradchikhi/VueServlet.java
@@ -0,0 +1,31 @@
+package edu.b2b.a18.projet_mouradchikhi;
+
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "VueServlet", value = "/vue-Servlet")
+public class VueServlet {
+    private String message;
+
+    public void init() {
+        message = "Bonjour de VueServlet";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/vue.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrateur
+  Date: 3/12/2025
+  Time: 1:09 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Vue</title>
+</head>
+<body>
+<h1>VueServlet</h1>
+<p>
+  Salutation de VueServlet
+</p>
+</body>
+</html>
